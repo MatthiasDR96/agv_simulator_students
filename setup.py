abb23a051c9b22c6a3b09ae2f2f1f990fc6c90c2
MatthiasDR96 <matthias.deryck@hotmail.com>
2019-11-19 11:51:25 +0100

    Last Changes

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='agv_simulator',
     version='1.0',
-    packages=[''],
+    packages=['src'],
     url='https://github.com/MatthiasDR96/agv_simulator.git',
     license='BSD',
     author='Matthias De Ryck',
     author_email='matthias.deryck@kuleuven.be',
     description='Python AGV simulator for research purposes.',
-    install_requires=['math', 'numpy', 'matplotlib', 'simpy', 'random', 'ortools']
+    install_requires=['math', 'numpy', 'matplotlib', 'simpy', 'random', 'ortools', 'configparser']
 )
